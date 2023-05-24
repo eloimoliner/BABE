3da4a84cdf31603325f98afd133b23e59849c9b5
Moliner Eloi <molinee2@login3.triton.aalto.fi>
2023-05-24 17:41:17 +0300

    dsd

--- testing_blindbwe.sh
+++ testing_blindbwe.sh
@@ -18,15 +18,15 @@ export CUDA_LAUNCH_BLOCKING=1
 n=$SLURM_ARRAY_TASK_ID
 
 
-n=54 #cqtdiff+ maestro 8s (alt version)
+#n=54 #cqtdiff+ maestro 8s (alt version)
 #n=65 #cocochorales strings
-#n=93 #cocochorales brass
+n=93 #cocochorales brass
 #n=94 #cocochorales woodwinf
 
 if [[ $n -eq 54 ]] 
 then
     #ckpt="/scratch/work/molinee2/projects/ddpm/blind_bwe_diffusion/experiments/54_maestro_22k/22k_8s-850000.pt"
-    ckpt="./experiments/54_maestro_22k/22k_8s-850000.pt"
+    ckpt="./experiments/maestro_piano/MAESTRO_22k_8s-850000.pt"
     exp=maestro22k_8s
     network=cqtdiff+
     #tester=blind_bwe_sweep
@@ -41,31 +41,28 @@ then
     diff_params=edm
 elif [[ $n -eq 65 ]] 
 then
-    ckpt="./experiments/65/22k_8s-190000.pt"
+    ckpt="./experiments/COCOChorales_strings/COCOChorales_strings_16k_11s-190000.pt"
     exp=CocoChorales_16k_8s
     network=cqtdiff+
     tester=blind_bwe_denoise_strings
-    #tester=edm_DC_correction_longer
     dset=CocoChorales_stems
     CQT=True
     diff_params=edm_chorales
 elif [[ $n -eq 93 ]] 
 then
-    ckpt="./experiments/93/22k_8s-480000.pt"
+    ckpt="./experiments/COCOChorales_woodwind/COCOChorales_woodwind_22k_8s-480000.pt"
     exp=CocoChorales_16k_8s
     network=cqtdiff+
     tester=blind_bwe_denoise_brass
-    #tester=edm_DC_correction_longer
     dset=CocoChorales_stems
     CQT=True
     diff_params=edm_chorales
 elif [[ $n -eq 94 ]] 
 then
-    ckpt="./experiments/94/22k_8s-390000.pt"
+    ckpt="./experiments/COCOChorales_brass/COCOChorales_brass_22k_8s-390000.pt"
     exp=CocoChorales_16k_8s
     network=cqtdiff+
     tester=blind_bwe_denoise_woodwind
-    #tester=edm_DC_correction_longer
     dset=CocoChorales_stems
     CQT=True
     diff_params=edm_chorales
@@ -78,7 +75,6 @@ PATH_EXPERIMENT=experiments/blind_bwe_tests/$n
 mkdir $PATH_EXPERIMENT
 
 
-#python train_w_cqt.py path_experiment="$PATH_EXPERIMENT"  $iteration
 python test.py model_dir="$PATH_EXPERIMENT" \
                dset=$dset \
                exp=$exp \
